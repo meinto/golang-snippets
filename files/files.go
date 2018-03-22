bf42937c35c500c48f4ea0512a51ca9385b10c8d
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2018-03-22 22:45:34 +0100

    adding support for relative paths

--- files/files.go
+++ files/files.go
@@ -4,30 +4,21 @@ import (
 	"fmt"
 	"path/filepath"
 	"os"
-	"log"
 )
-
-
-func executionPath() (string, error) {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(dir)
-	return dir, err
-}
 
 
 func List(path string) ([]File, error) {
 
 	files := make([]File, 0)
 
-	// isAbs := filepath.IsAbs(path)
+	isAbs := filepath.IsAbs(path)
 
-	dir, err := executionPath()
+	var err error
+	if !isAbs {
+		path, err = filepath.Abs(path)
+	}
+	
 	if err == nil {
-		fmt.Println(dir)
-
 		err = filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
 			if err == nil {
 				files = append(files, File{
