599dd84a7d6bd3d0d729135abd5f0eea05c8df41
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2018-03-22 23:22:12 +0100

    adding functions for opening and deleting file

--- files/type_file.go
+++ files/type_file.go
@@ -1,5 +1,9 @@
 package files
 
+import (
+	"os"
+	"fmt"
+)
 
 type File struct{
 	path string
@@ -8,3 +12,19 @@ type File struct{
 func (f File) ToString() string {
 	return f.path
 }
+
+func (f File) Open() (*os.File, error) {
+	file, err := os.Open(f.path)
+	return file, err
+}
+
+func (f File) Delete() error {
+	err := os.Remove(f.path)
+	if err != nil {
+		fmt.Println("couldn't delete file:", f.path)
+	} else {
+		fmt.Println("file sucessfully deleted", f.path)
+	}
+	return err
+}
+
